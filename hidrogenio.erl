889e23d1a82270a494d3a9e45a7135b4b81896c4
Rafael Johann <rafael.johann2@gmail.com>
2021-11-15 01:15:12 -0300

    Criação das moléculas de hidrogênio e oxigênio após timer
    Signed-off-by: Rafael Johann <rafael.johann2@gmail.com>

--- hidrogenio.erl
+++ hidrogenio.erl
@@ -1,5 +1,32 @@
 -module(hidrogenio).
--export([hidrogenio/0]).
+-export([start/1, criaMoleculaHidrogenio/2, verificarCriouHidrogenio/0]).
 
-hidrogenio()->
-    io:format("hidrogenio teste", []).
+start(Num) when not is_number(Num)-> {error,not_a_number};
+start(Num)->spawn(fun()->criaMoleculaHidrogenio(1, Num) end).
+
+criaMoleculaHidrogenio(Contador, Maximo)->
+    
+	erlang:start_timer(1000, self(), []),
+    receive
+          {timeout, Tref, _} ->
+            erlang:cancel_timer(Tref),
+		
+			io:format("Contador: ~p~n", [Contador]),
+			io:format("Maximo: ~p~n", [Maximo]),
+			
+			if
+				Contador >= 10 -> CriouHidrogenio = verificarCriouHidrogenio();
+				Contador == 30 -> CriouHidrogenio = 1;
+				true -> CriouHidrogenio = 0
+			end,
+			
+			io:format("CRIOU~p~n", [CriouHidrogenio]),
+			
+            if
+                (Contador =:=Maximo) or (CriouHidrogenio =:= 5) -> io:format("Criei molécula de hidrogênio!~n");
+                true -> criaMoleculaHidrogenio(Contador+1, Maximo)
+            end
+    end.
+	
+verificarCriouHidrogenio() ->
+	rand:uniform(5).
--- oxigenio.erl
+++ oxigenio.erl
@@ -1,5 +1,32 @@
 -module(oxigenio).
--export([oxigenio/0]).
+-export([start/1, criaMoleculaOxigenio/2, verificarCriouOxigenio/0]).
 
-oxigenio()->
-    io:format("oxigenio teste", []).
+start(Num) when not is_number(Num)-> {error,not_a_number};
+start(Num)->spawn(fun()->criaMoleculaOxigenio(1, Num) end).
+
+criaMoleculaOxigenio(Contador, Maximo)->
+    
+	erlang:start_timer(1000, self(), []),
+    receive
+          {timeout, Tref, _} ->
+            erlang:cancel_timer(Tref),
+		
+			io:format("Contador: ~p~n", [Contador]),
+			io:format("Maximo: ~p~n", [Maximo]),
+			
+			if
+				Contador >= 10 -> CriouOxigenio = verificarCriouOxigenio();
+				Contador == 30 -> CriouOxigenio = 1;
+				true -> CriouOxigenio = 0
+			end,
+			
+			io:format("CRIOU~p~n", [CriouOxigenio]),
+			
+            if
+                (Contador =:=Maximo) or (CriouOxigenio =:= 5) -> io:format("Criei molécula de oxigênio!~n");
+                true -> criaMoleculaOxigenio(Contador+1, Maximo)
+            end
+    end.
+	
+verificarCriouOxigenio() ->
+	rand:uniform(5).
